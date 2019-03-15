ee0f11a11bf73ccadecb3629c3ec0624cd26a7fd
rndev80 <rndev80@gmail.com>
2019-03-15 23:11:37 +0100

    Add devise-token-auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
 
     def index
         @appointments = Appointments.order('appt_time ASC')
